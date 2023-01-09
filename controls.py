7b1bf2dc09f06d2c32c2e29feb8e3ad6a0044dc5
streizz <88502728+streizz@users.noreply.github.com>
2023-01-09 18:51:51 +0300

    Update controls.py

--- controls.py
+++ controls.py
@@ -11,14 +11,14 @@ def process_events(game):
                 game.gun.mright = True
             elif event.key == pygame.K_LEFT:
                 game.gun.mleft = True
-            elif event.key == pygame.K_UP:
+            elif event.key == pygame.K_SPACE:
                 game.gun.shooting = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 game.gun.mright = False
             elif event.key == pygame.K_LEFT:
                 game.gun.mleft = False
-            elif event.key == pygame.K_UP:
+            elif event.key == pygame.K_SPACE:
                 game.gun.shooting = False
 
 
